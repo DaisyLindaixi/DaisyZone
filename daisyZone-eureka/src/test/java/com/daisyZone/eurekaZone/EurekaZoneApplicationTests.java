bc361d48ecf19e54b6d94561e24258f1fd9696c5
DaisyLindaixi <43634724+DaisyLindaixi@users.noreply.github.com>
2018-09-27 16:49:17 +0800

    Add files via upload

---
+++ daisyZone-eureka/src/test/java/com/daisyZone/eurekaZone/EurekaZoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daisyZone.eurekaZone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaZoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
